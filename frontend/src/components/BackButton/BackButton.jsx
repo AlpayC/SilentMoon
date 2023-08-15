c6e5b047daa454fedda3f46ab09d74d64c8c4d11
mirzakomic <komic.mirza@gmail.com>
2023-08-15 14:39:51 +0200

    back button funktion wurde aktualisiert

--- frontend/src/components/BackButton/BackButton.jsx
+++ frontend/src/components/BackButton/BackButton.jsx
@@ -1,10 +1,15 @@
 import backImg from "../../assets/img/Icons/back.svg"
 import "../BackButton/BackButton.css"
+import { useNavigate } from "react-router-dom";
 
 const BackButon = () => {
+  const navigate = useNavigate();
+	const goBack = () => {
+		navigate(-1);
+	}
   return (
     <>
-      <button className="back-btn"><img src={backImg}/></button>
+      <button onClick={goBack} className="back-btn"><img src={backImg}/></button>
     </>
   );
 };
