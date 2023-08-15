93d48081284bbd4cf9c2c69e089cc052f69f04ef
AlpayC <alpaycelik91@googlemail.com>
2023-08-15 13:22:55 +0200

    added context for pexel, spotify and userdata

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,6 +1,21 @@
 import "./Profile.css";
+import { GeneratePexelDataContext } from "../../context/GeneratePexelDataContext";
+
+import { useContext, useEffect } from "react";
+import { useUserData } from "../../context/UserDataContext";
 
 const Profile = () => {
+  const { pexelData } = useContext(GeneratePexelDataContext);
+  const { userData } = useUserData();
+
+  console.log(userData);
+  useEffect(() => {
+    console.log(pexelData);
+  }, [pexelData]);
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
   return (
     <>
       <h1>Profile</h1>
--- frontend/src/pages/WelcomePage/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,13 @@
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserData } from "../../context/UserDataContext";
+
 const WelcomePage = () => {
+  const { userData } = useUserData();
+  console.log(userData);
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome {userData?.email}</h1>
     </>
   );
 };
