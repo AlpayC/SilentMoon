66c2d689669c1dffc01dbdb5912167cb54879c29
mirzakomic <komic.mirza@gmail.com>
2023-08-15 13:59:58 +0200

    Button, BackButton, SignUp, SignIn, Index.css geadded, geändert

--- frontend/src/user/Signin.jsx
+++ frontend/src/user/Signin.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import BackButon from "../components/BackButton/BackButton.jsx";
+import Button from "../components/Button/Button.jsx";
 
 export default function Signin() {
   const { refetch } = useContext(UserContext);
@@ -26,11 +28,18 @@ export default function Signin() {
   };
 
   return (
-    <form onSubmit={submit}>
-      <input name="email" type="email" placeholder="your email" />
-      <input name="password" type="password" placeholder="***********" />
-      {error && <small style={{ color: "red" }}>{error}</small>}
-      <button>Login</button>
-    </form>
+    <div className="main-wrapper background-signinup">
+      <BackButon />
+      <h1 className="center padding-top-bottom">Welcome Back!</h1>
+      <form className="column" onSubmit={submit}>
+        <input name="email" type="email" placeholder="EMAIL" />
+        <input name="password" type="password" placeholder="PASSWORD" />
+        {error && <small style={{ color: "red" }}>{error}</small>}
+        <Button text="login" />
+        <p className="center grey-text">
+          DON'T HAVE AN ACCOUNT YET? <Link to={"../signup"}>SIGN UP</Link>
+        </p>
+      </form>
+    </div>
   );
 }
