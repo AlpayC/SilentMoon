97851fe5d20834ccb0d31e6f0643540ea7fee805
AlpayC <alpaycelik91@googlemail.com>
2023-08-16 17:06:41 +0200

    fixed corrupted cloudinary links and nav link meditation

--- frontend/src/pages/CategoryMeditate/CategoryMeditate.jsx
+++ frontend/src/pages/CategoryMeditate/CategoryMeditate.jsx
@@ -1,9 +1,11 @@
+import NavBar from "../../components/NavBar/NavBar";
 import "./CategoryMeditate.css";
 
 const CategoryMeditate = () => {
   return (
     <>
       <h1>Mediate</h1>
+      <NavBar />
     </>
   );
 };
