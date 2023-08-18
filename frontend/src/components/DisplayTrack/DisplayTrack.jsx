6c5f05e808523b612bbd8a03feafbeeda97844bf
Lis-art <lkesselheim@gmail.com>
2023-08-18 15:12:30 +0200

    displayTrack component fixed

--- frontend/src/components/DisplayTrack/DisplayTrack.jsx
+++ frontend/src/components/DisplayTrack/DisplayTrack.jsx
@@ -6,11 +6,12 @@ const DisplayTrack = ({
   audioRef,
   setDuration,
   progressBarRef,
+  trackItemData,
 }) => {
   const onLoadedMetadata = () => {
     const seconds = audioRef.current.duration;
     setDuration(seconds);
-    progressBarRef.current.max = seconds;
+    //progressBarRef.current.max = seconds;
   };
 
   useEffect(() => {
@@ -23,12 +24,12 @@ const DisplayTrack = ({
 
   return (
     <div>
-      <div className="text">
+      {/*       <div className="text">
         <p className="title">{currentTrack.title}</p>
-      </div>
-      <audio ref={audioRef} onLoadedMetadata={onLoadedMetadata} controls />
-      {/* <source src={track.preview_url} type="audio/mp3" /> */}
-      {/* <p>0rSpNi9235sKXcd2wBaMkz</p> */}
+      </div> */}
+      <audio ref={audioRef} onLoadedMetadata={onLoadedMetadata} controls>
+        <source src={trackItemData?.preview_url} type="audio/mp3" />
+      </audio>
     </div>
   );
 };
