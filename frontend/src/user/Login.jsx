6f4ac866af007bd8d53c973f15fe3d5251c1277b
Lis-art <lkesselheim@gmail.com>
2023-08-20 19:11:50 +0200

    player component finished+meditationplayer page+CSS HomePage

--- frontend/src/user/Login.jsx
+++ frontend/src/user/Login.jsx
@@ -5,34 +5,35 @@ import LoginImage from "../assets/img/Login/bg-person-handstand.png";
 import "./Login.css";
 
 const Login = () => {
-
-	return (
-		<>
-			<section>
-				<div
-					className='loginPage'
-					style={{ backgroundImage: `url(${LoginImage})` }}
-				>
-					<Logo className='logo-black main-wrapper' />
-				</div>
-			</section>
-			<section className='loginContainer'>
-				<h1>We are what we do</h1>
-				<p>
-					Thousand of people are using silent moon for meditation and yoga
-					classes.
-				</p>
-				<div className='signingLinks'>
-					<NavLink to={"./signup"} className='navLink'>
-						<Button text='SIGN UP' />
-					</NavLink>
-					<NavLink to={"./signin"} className='navLink'>
-						ALREADY HAVE AN ACCOUNT? <span>LOG IN</span>
-					</NavLink>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section>
+        <section
+          className="loginPage"
+          style={{ backgroundImage: `url(${LoginImage})` }}
+        >
+          <div>
+            <Logo className="logo-black main-wrapper" />
+          </div>
+        </section>
+        <section className="loginContainer">
+          <h1>We are what we do</h1>
+          <p>
+            Thousand of people are using silent moon for meditation and yoga
+            classes.
+          </p>
+          <div className="signingLinks">
+            <NavLink to={"./signup"} className="navLink">
+              <Button text="SIGN UP" />
+            </NavLink>
+            <NavLink to={"./signin"} className="navLink">
+              ALREADY HAVE AN ACCOUNT? <span>LOG IN</span>
+            </NavLink>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Login;
