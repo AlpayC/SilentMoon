e287a3c587c28787decc5107471776588b09db96
Lis-art <lkesselheim@gmail.com>
2023-08-21 15:04:55 +0200

    logout Btn

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -26,11 +26,9 @@ const Profile = () => {
   );
   const [searchInput, setSearchInput] = useState("");
 
-
   const favoriteExercises = exerciseData?.data || storagedExerciseData?.data;
 
   const { isLoggedIn, logout } = useContext(UserContext);
-
 
   const favoriteVideos = favoriteExercises?.filter((video) =>
     storagedUserData.videos.includes(video._id)
@@ -40,49 +38,54 @@ const Profile = () => {
 
   return (
     <>
-          {isLoggedIn && (
-      <div className="main-wrapper">
-        <h1>{userData?.name || storagedUserData?.name}</h1>
-        <SearchBar
-          value={searchInput}
-          onChange={(e) => {
-            setSearchInput(e.target.value);
-          }}
-        />
-        <Stats />
-        <h2>Favourite Yoga Sessions</h2>
-        <section className="slider">
-          {favoriteVideos?.map((item) => (
-            <RecommendedItem
-              key={item._id}
-              link={`/category/yoga/${item._id}`}
-              image={item.image_url}
-              title={item.title}
-              level={item.level}
-              duration={item.duration}
-            />
-          ))}
-        </section>
-        <h2>Favourite Meditations</h2>
-        <section className="slider">
-          {favoritePlaylists?.map((item) => (
-            <RecommendedItem
-              key={item.id}
-              link={`/category/meditation/${item.id}`}
-              title={
-                item.name.length > 20
-                  ? `${item.name.substring(0, 10)}`
-                  : item.name
-              }
-              playlist_id={item.id}
-              image={item?.images[0].url}
-              tracks={item?.tracks.total}
-              owner={item?.owner.display_name}
-            />
-          ))}
-        </section>
-        <NavBar />
-      </div>
+      {isLoggedIn && (
+        <div className="main-wrapper">
+          <div>
+            <h1>{userData?.name || storagedUserData?.name}</h1>
+            <button onClick={logout}>
+              <img src={LogoutBtn} alt="logout button" />
+            </button>
+          </div>
+          <SearchBar
+            value={searchInput}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+            }}
+          />
+          <Stats />
+          <h2>Favourite Yoga Sessions</h2>
+          <section className="slider">
+            {favoriteVideos?.map((item) => (
+              <RecommendedItem
+                key={item._id}
+                link={`/category/yoga/${item._id}`}
+                image={item.image_url}
+                title={item.title}
+                level={item.level}
+                duration={item.duration}
+              />
+            ))}
+          </section>
+          <h2>Favourite Meditations</h2>
+          <section className="slider">
+            {favoritePlaylists?.map((item) => (
+              <RecommendedItem
+                key={item.id}
+                link={`/category/meditation/${item.id}`}
+                title={
+                  item.name.length > 20
+                    ? `${item.name.substring(0, 10)}`
+                    : item.name
+                }
+                playlist_id={item.id}
+                image={item?.images[0].url}
+                tracks={item?.tracks.total}
+                owner={item?.owner.display_name}
+              />
+            ))}
+          </section>
+          <NavBar />
+        </div>
       )}
     </>
   );
